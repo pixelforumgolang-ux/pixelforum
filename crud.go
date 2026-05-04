6797b4e506a667ea1bf79d6e595ab65e37272f49
goult280-afk <goult280@gmail.com>
2026-05-04 15:01:57 +0200

    correction crud

--- crud.go
+++ crud.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"time"
+	"fmt"
 )
 
 //---------------
@@ -11,79 +12,113 @@ import (
 
 //CREATE RUD
 
-func createUser(&User, userName string, userMail string, userMdp string, lastName string, firstName string){
+func createUser(userName string, userMail string, userMdp string, lastName string, firstName string){
 	
 	if (userName == "" || userMail == "" || userMdp == "" || lastName == "" || firstName  == "" ){
-		return "no complete information"
+		return nil
 	} else {
-		return db.Create(&User, userName, userMail, userMdp, "client", lastName, firstName)
+		user := User{UserName: userName, 
+			FirstName : firstName, 
+			LastName : LastName, 
+			UserMail = userMail,   	
+			UserMdp = userMdp, 	
+			UserStatus = "client"}
+
+		result := db.Create(&user)
+
+		if (result.Error != nil) {
+			fmt.println("erreur")
+		}
 	}
+}
+
+func createPost(theIdUser uint, postMessage string, postImg string, postDate time.time, list []uint){
+
+	if (PostMessage == ""){
+		fmt.println("pas de message")
+	} else {
+		post := Post{		
+				idUser : theIdUser,		
+				PostMessage : postMessage,
+				PostImg : postImg,
+				PostDate : postDate,	
+				PostCommentary : list}
+
+		result := db.Create(&post)
+
+		if (result.Error != nil) {
+			fmt.println("erreur")
+		}
+	}
+
 	
 }
 
-func createPost(&Post, idUser uint, User User, PostMessage string, PostImg string, PostDate time.time){
-
-	if (PostMessage == ""){
-		return "no complete information"
-	} else {
-		if ( PostImg == nil) {
-			PostImg = ""
-		}
-		return db.Create(&Post, idUser, User, PostMessage, PostImg, PostDate, [])
-	}
-}
-
-func createCommentary(&Commentary, idUser uint, User User,  ComMessage string, ComImg string, ComDate time.time){
+func createCommentary(theIdUser uint,  ComMessage string, ComImg string, ComDate time.time){
 	if (ComMessage == ""){
-		return "no complete information"
+		fmt.println("pas de message")
 	} else {
-		if ( ComImg == nil) {
-			ComImg = ""
+		com := Commentary{		
+			UserComId : theIdUser,		
+			Message : comMessage,
+			Image  : postImg,
+			comDate : comDateDate}
+
+		result := db.Create(&com)
+
+		if (result.Error != nil) {
+			fmt.println("erreur")
+		} else {
+			fmt.println(result.RowsAffected)
 		}
-		return db.Create(&Commentary, idUser, User, ComMessage, ComImg, ComDate)
 	}
 }
 
 //C READ UD
 
-func readUser(id uint, &User){
-	return db.Find(&User, "id = ?", id)
+func readUser(id uint, theUser User){
+	result := db.First(&theUser)
 }
 
-func readPost(id uint, &Post){
-	return db.Find(&Post, "id = ?", id)
+func readPost(id uint, thePost Post){
+	result := db.First(&thePost)
 }
 
-func readCommentary(idList []uint, &Commentary){
+func readCommentary(idList []uint, CommentaryList []Commentary){
 	if (idList > 0){
-		return db.Find(&Commentary, "Commentary.Commentary_id IN ?", idList)
-	} else {
-		return "no commentary"
-	}
-	
+		result := db.Find(&CommentaryList, idList)
+	} 
 }
+
 
 //CR UPDATE D
 
 func updateUser(id uint, userName string, userMail string, userMdp string, userStatus string, lastName string, firstName string){
-	return db.Model(&User).Update(id, userName, userMail, userMdp, userStatus, lastName, firstName)
+	dataUser := User{UserName: userName, 
+			FirstName : firstName, 
+			LastName : LastName, 
+			UserMail : userMail,	
+			UserMdp : userMdp,
+			UserStatus : userStatus}
+	
+	result := db.Model(User{}).Where("id = ?", id).Updates(dataUser)
 }
 
-func updatePost(id uint, idUser uint, User User, PostMessage string, PostImg string, PostDate time.time, PostCommentary []uint){
-	return db.Model(&Post).Update(id, idUser, User, PostMessage, PostImg, PostDate, PostCommentary)
+func updatePost(id uint, postCommentary []uint){
+	result := db.Model(Post{}).Where("id = ?", id).Updates(Post{PostCommentary : postCommentary})
 }
 
 
 //CRU DELETE
 
 func deleteUser(id uint){
-	return db.Where("id = ?", id).Delete(&User)
+	result := db.Delete(&User{}, id)
 }
 
 func deletePost(id uint){
-	return db.Where("idPost = ?", id).Delete(&Post)
+	result := db.Delete(&Post{}, id)
 }
 
 func deleteCom(id uint){
-	return db.Where("idCom = ?", id).Delete(&Com)
+	result := db.Delete(&Commentary{}, id)
 }
