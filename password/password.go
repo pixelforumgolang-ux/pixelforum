8f1df2f6b64827e626ae37828aa7a8025fdce8d1
Benoit <benoit.eberhard@gmail.com>
2026-05-05 14:55:58 +0200

    generateur-mdp

---
+++ password/password.go
@@ -0,0 +1,30 @@
+package password
+import (
+    "math/rand"
+    "time"
+)
+const (
+    lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits           = "0123456789"
+    specialChars     = "!@#$%^&*()-_=+[]{}|;:'\"<>,.?/~"
+)
+func GeneratePassword(length int, useUppercase, useDigits, useSpecialChars bool) string {
+    var validChars string
+    validChars += lowercaseLetters
+    if useUppercase {
+        validChars += uppercaseLetters
+    }
+    if useDigits {
+        validChars += digits
+    }
+    if useSpecialChars {
+        validChars += specialChars
+    }
+    rand.Seed(time.Now().UnixNano())
+    password := make([]byte, length)
+    for i := 0; i < length; i++ {
+        password[i] = validChars[rand.Intn(len(validChars))]
+    }
+    return string(password)
+}
